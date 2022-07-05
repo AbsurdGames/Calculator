0db31a7befe479be92a9110ad329b218dca6f431
Absurd Games Dev <104153005+AbsurdGames@users.noreply.github.com>
2022-07-05 12:20:18 +0200

    Update tkCalculator.py

--- tkCalculator.py
+++ tkCalculator.py
@@ -9,7 +9,7 @@ except Exception as E:
     quit()
 
 screen = Tk()
-screen.title("Calcularor - Beta1.2.0")
+screen.title("Calcularor - Beta1.2.7")
 screen.geometry("290x520")
 screen.configure(bg="white")
 screen.eval("tk::PlaceWindow . center")
@@ -24,11 +24,280 @@ while True:
     try:
         def close(event):
             quit()
+            
+        def keyPress():
+            def one_(event):
+                """In all number def's"""
+                
+                global value, value2, digit
+                
+                if digit == 1:
+                    value = value * 10 + 1
+
+                elif digit == 2:
+                    value2 = value2 * 10 + 1
+
+
+                print (value)
+
+            def two_(events):
+                """In all number def's"""
+                
+                global value, value2, digit
+
+                if digit == 1:
+                    value = value * 10 + 2
+
+                elif digit == 2:
+                    value2 = value2 * 10 + 2
+
+                print (value)
+
+            def three_(event):
+                """In all number def's"""
+                
+                global value, value2, digit
+
+                if digit == 1:
+                    value = value * 10 + 3
+
+                elif digit == 2:
+                    value2 = value2 * 10 + 3
+
+                print (value)
+
+            def four_(event):
+                """In all number def's"""
+                
+                global value, value2, digit
+
+                if digit == 1:
+                    value = value * 10 + 4
+
+                elif digit == 2:
+                    value2 = value2 * 10 + 4
+
+                print (value)
+
+            def five_(event):
+                """In all number def's"""
+                
+                global value, value2, digit
+
+                if digit == 1:
+                    value = value * 10 + 5
+
+                elif digit == 2:
+                    value2 = value2 * 10 + 5
+
+                print (value)
+
+            def six_(event):
+                """In all number def's"""
+                
+                global value, value2, digit
+                if digit == 1:
+                    value = value * 10 + 6
+
+                elif digit == 2:
+                    value2 = value2 * 10 + 6
+
+                print (value)
+
+            def seven_(event):
+                """In all number def's"""
+                
+                global value, value2, digit
+
+                if digit == 1:
+                    value = value * 10 + 7
+
+                elif digit == 2:
+                    value2 = value2 * 10 + 7
+                
+                print (value)
+
+            def eight_(event):
+                """In all number def's"""
+                
+                global value, value2, digit
+
+                if digit == 1:
+                    value = value * 10 + 8
+
+                elif digit == 2:
+                    value2 = value2 * 10 + 8
+
+                print (value)
+
+            def nine_(event):
+                """In all number def's"""
+                
+                global value, value2, digit
+
+                if digit == 1:
+                    value = value * 10 + 9
+
+                elif digit == 2:
+                    value2 = value2 * 10 + 9
+
+                print (value)
+
+            def zero_(event):
+                """In all number def's"""
+                
+                global value, value2, digit
+
+                if digit == 1:
+                    value = value * 10 + 0
+
+                elif digit == 2:
+                    value2 = value2 * 10 + 0
+
+                print (value)
+
+            def plus_(event):
+                global digit, operation
+
+                operation = "sum"
+                digit = 2
+
+                print (digit)
+                
+            def minus_(event):
+                global digit, operation
+
+                operation = "minus"
+                digit = 2
+
+                print (digit)
+                
+            def divide_(event):
+                global digit, operation
+
+                operation = "divide"
+                digit = 2
+
+                print (digit)
+                
+            def ExactDivide(event):
+                global digit, operation
+
+                operation = "Edivide"
+                digit = 2
+
+                print (digit)
+                
+            def multi_(event):
+                global digit, operation
+
+                operation = "multi"
+                digit = 2
+
+                print (digit)
+                
+            def percent_ (event):
+                global digit, operation
+                
+                operation = "percent"
+                digit = 2
+
+            def equal_(event):
+                global total, digit, operation, value, value2
+
+                if operation == "sum":
+                    total = value + value2
+                    
+                elif operation == "minus":
+                    total = value - value2
+                    
+                elif operation == "divide":
+                    total = value / value2
+                    
+                elif operation == "Edevide":
+                    total = value // value2
+                    
+                elif operation == "multi":
+                    total = value * value2
+                    
+                elif operation == "percent":
+                    try:
+                        total = value % value
+                    
+                    except:
+                        total = 0
+                    
+                elif operation == "":
+                    operation = operation
+                
+                else:
+                    print ("There is an error in operatons")
+                    
+                digit = 2
+                
+                total = ""
+                operation = ""
+                
+                value = 0
+                value2 = 0
+
+                print(total)
+
+            def clearAll_(event):
+                global value, value2, digit
+                
+                if digit == 1:
+                    value = 0
+                    value2 = 0
+                    
+                    digit = 1
+                    
+            def clear_(event):
+                global value, value2, digit
+                
+                if digit == 2:
+                    value2 = 0
+
+                elif digit == 1:
+                    value = 0
+                    
+            def pm_ (event):
+                global value, value2, digit
+                
+                if digit == 1:
+                    value = value * -3
+                    
+                elif digit == 2:
+                    value2 = value2 * -3
+                    
+                print (value)
+                
+            screen.bind("1", one_)
+            screen.bind("2", two_)
+            screen.bind("3", three_)
+            screen.bind("4", four_)
+            screen.bind("5", five_)
+            screen.bind("6", six_)
+            screen.bind("7", seven_)
+            screen.bind("8", eight_)
+            screen.bind("9", nine_)
+            screen.bind("0", zero_)
+                
+            screen.bind("+", plus_)
+            screen.bind("-", minus_)
+            screen.bind("/", divide_)
+            screen.bind("\\", ExactDivide)
+            screen.bind("*", multi_)
+            screen.bind("%", percent_)
+            screen.bind("=", equal_)
+            screen.bind("<c>", clear_)
+            screen.bind("<a>", clearAll_)
+            screen.bind("<m>", pm_)
         
         def one_():
             """In all number def's"""
             
-            global value, value2
+            global value, value2, digit
             
             if digit == 1:
                 value = value * 10 + 1
@@ -42,7 +311,7 @@ while True:
         def two_():
             """In all number def's"""
             
-            global value, value2
+            global value, value2, digit
 
             if digit == 1:
                 value = value * 10 + 2
@@ -55,7 +324,7 @@ while True:
         def three_():
             """In all number def's"""
             
-            global value, value2
+            global value, value2, digit
 
             if digit == 1:
                 value = value * 10 + 3
@@ -68,7 +337,7 @@ while True:
         def four_():
             """In all number def's"""
             
-            global value, value2
+            global value, value2, digit
 
             if digit == 1:
                 value = value * 10 + 4
@@ -81,7 +350,7 @@ while True:
         def five_():
             """In all number def's"""
             
-            global value, value2
+            global value, value2, digit
 
             if digit == 1:
                 value = value * 10 + 5
@@ -94,7 +363,7 @@ while True:
         def six_():
             """In all number def's"""
             
-            global value, value2
+            global value, value2, digit
             if digit == 1:
                 value = value * 10 + 6
 
@@ -106,7 +375,7 @@ while True:
         def seven_():
             """In all number def's"""
             
-            global value, value2
+            global value, value2, digit
 
             if digit == 1:
                 value = value * 10 + 7
@@ -119,7 +388,7 @@ while True:
         def eight_():
             """In all number def's"""
             
-            global value, value2
+            global value, value2, digit
 
             if digit == 1:
                 value = value * 10 + 8
@@ -132,7 +401,7 @@ while True:
         def nine_():
             """In all number def's"""
             
-            global value, value2
+            global value, value2, digit
 
             if digit == 1:
                 value = value * 10 + 9
@@ -145,7 +414,7 @@ while True:
         def zero_():
             """In all number def's"""
             
-            global value, value2
+            global value, value2, digit
 
             if digit == 1:
                 value = value * 10 + 0
@@ -159,6 +428,7 @@ while True:
             global digit, operation
 
             operation = "sum"
+            digit = 2
 
             print (digit)
             
@@ -166,6 +436,7 @@ while True:
             global digit, operation
 
             operation = "minus"
+            digit = 2
 
             print (digit)
             
@@ -173,6 +444,7 @@ while True:
             global digit, operation
 
             operation = "divide"
+            digit = 2
 
             print (digit)
             
@@ -180,6 +452,7 @@ while True:
             global digit, operation
 
             operation = "Edivide"
+            digit = 2
 
             print (digit)
             
@@ -187,6 +460,7 @@ while True:
             global digit, operation
 
             operation = "multi"
+            digit = 2
 
             print (digit)
             
@@ -194,6 +468,7 @@ while True:
             global digit, operation
             
             operation = "percent"
+            digit = 2
 
         def equal_():
             global total, digit, operation, value, value2
@@ -226,7 +501,7 @@ while True:
             else:
                 print ("There is an error in operatons")
                 
-            digit += 1
+            digit = 2
             
             total = ""
             operation = ""
@@ -237,15 +512,15 @@ while True:
             print(total)
 
         def clearAll_():
-            global value, value2
-            if digit == 1:
-                value = 0
-                value2 = 0
-                
-                digit = 1
+            global value, value2, digit
+
+            value = 0
+            value2 = 0
+            
+            digit = 1
                 
         def clear_():
-            global value, value2
+            global value, value2, digit
             
             if digit == 2:
                 value2 = 0
@@ -254,7 +529,7 @@ while True:
                 value = 0
                 
         def pm_ ():
-            global value, value2
+            global value, value2, digit
             
             if digit == 1:
                 value = value * -3
@@ -361,6 +636,8 @@ while True:
         pm.grid(row=5, column=1)
         percent.grid(row=4, column=2)
             
+        keyPress()
+        
         screen.bind("<Escape>", close)
         
         screen.mainloop()
